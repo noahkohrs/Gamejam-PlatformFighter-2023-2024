00490e8b5bf6c78e4fd57d0ea27ea4178ee2cac6
Lambert Bastien <bastien.lambert@univ-grenoble-alpes.fr>
2023-06-13 14:36:30 +0200

    Changed HitBoxView Color
    Changed color to RED for better visibility

--- info3.game/game/info3/game/entity/HitBoxView.java
+++ info3.game/game/info3/game/entity/HitBoxView.java
@@ -1,5 +1,6 @@
 package info3.game.entity;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 import info3.game.Camera;
@@ -14,8 +15,8 @@ public class HitBoxView {
     }
 
     public void paint(Graphics g) {
+        g.setColor(Color.RED);
         Camera.drawRect(g, hitBox.getTopX(), hitBox.getTopY(), hitBox.width, hitBox.height);
       }
 
-      
 }
