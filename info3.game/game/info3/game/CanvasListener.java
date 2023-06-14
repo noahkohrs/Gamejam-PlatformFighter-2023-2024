bb5bccd58fd7878f20afa3a2dfc77c3e38915592
Lambert Bastien <bastien.lambert@univ-grenoble-alpes.fr>
2023-06-14 11:38:07 +0200

    Added movement with Direction & INDENT

--- info3.game/game/info3/game/CanvasListener.java
+++ info3.game/game/info3/game/CanvasListener.java
@@ -24,6 +24,7 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import info3.game.entity.Direction;
 import info3.game.graphics.GameCanvasListener;
 
 public class CanvasListener implements GameCanvasListener {
@@ -35,74 +36,86 @@ public class CanvasListener implements GameCanvasListener {
 
   @Override
   public void mouseClicked(MouseEvent e) {
-    System.out.println("Mouse clicked: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse clicked: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mousePressed(MouseEvent e) {
-    System.out.println("Mouse pressed: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse pressed: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mouseReleased(MouseEvent e) {
-    System.out.println("Mouse released: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse released: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mouseEntered(MouseEvent e) {
-    System.out.println("Mouse entered: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse entered: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mouseExited(MouseEvent e) {
-    System.out.println("Mouse exited: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse exited: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mouseDragged(MouseEvent e) {
-    System.out.println("Mouse dragged: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse dragged: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void mouseMoved(MouseEvent e) {
-    System.out.println("Mouse moved: ("+e.getX()+","+e.getY()+")");
-    System.out.println("   modifiers="+e.getModifiersEx());
-    System.out.println("   buttons="+e.getButton());
+    System.out.println("Mouse moved: (" + e.getX() + "," + e.getY() + ")");
+    System.out.println("   modifiers=" + e.getModifiersEx());
+    System.out.println("   buttons=" + e.getButton());
   }
 
   @Override
   public void keyTyped(KeyEvent e) {
-    System.out.println("Key typed: "+e.getKeyChar()+" code="+e.getKeyCode());
+    System.out.println("Key typed: " + e.getKeyChar() + " code=" + e.getKeyCode());
   }
 
   @Override
   public void keyPressed(KeyEvent e) {
-    System.out.println("Key pressed: "+e.getKeyChar()+" code="+e.getKeyCode());
-    switch (e.getKeyCode()) { 
-    case KeyEvent.VK_SPACE:
-      GameSession.gameSession.camera.toggleDebugMode();
-      break;
-    case KeyEvent.VK_Q:
-    GameSession.gameSession.player1.takeDamage(10);
-    break;
+    System.out.println("Key pressed: " + e.getKeyChar() + " code=" + e.getKeyCode());
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_SPACE:
+        GameSession.gameSession.camera.toggleDebugMode();
+        break;
+      case KeyEvent.VK_T:
+        GameSession.gameSession.player1.takeDamage(10);
+        break;
+      case KeyEvent.VK_Z:
+        GameSession.gameSession.player1.move(Direction.UPPER);
+        break;
+      case KeyEvent.VK_D:
+        GameSession.gameSession.player1.move(Direction.RIGHT);
+        break;
+      case KeyEvent.VK_S:
+        GameSession.gameSession.player1.move(Direction.BOTTOM);
+        break;
+      case KeyEvent.VK_Q:
+        GameSession.gameSession.player1.move(Direction.LEFT);
+        break;
     }
   }
 
   @Override
   public void keyReleased(KeyEvent e) {
-    System.out.println("Key released: "+e.getKeyChar()+" code="+e.getKeyCode());
+    System.out.println("Key released: " + e.getKeyChar() + " code=" + e.getKeyCode());
   }
 
   @Override
@@ -118,27 +131,27 @@ public class CanvasListener implements GameCanvasListener {
   @Override
   public void windowOpened() {
     m_game.loadMusic();
-//    m_game.m_canvas.setTimer(6000);
+    // m_game.m_canvas.setTimer(6000);
   }
 
   @Override
   public void exit() {
   }
 
-//  boolean m_expired;
+  // boolean m_expired;
   @Override
   public void endOfPlay(String name) {
-//    if (!m_expired) // only reload if it was a forced reload by timer
-      m_game.loadMusic();
-//    m_expired = false;
+    // if (!m_expired) // only reload if it was a forced reload by timer
+    m_game.loadMusic();
+    // m_expired = false;
   }
 
   @Override
-  public void expired() { 
+  public void expired() {
     // will force a change of music, after 6s of play
-//    System.out.println("Forcing an ealy change of music");
-//    m_expired = true;
-//    m_game.loadMusic();    
+    // System.out.println("Forcing an ealy change of music");
+    // m_expired = true;
+    // m_game.loadMusic();
   }
 
 }
