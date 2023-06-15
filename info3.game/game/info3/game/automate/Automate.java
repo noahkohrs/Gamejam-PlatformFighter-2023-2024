5e0ba151c80a92b73ffab85355a541ab15f5e9c8
Clement Dehosse <clement.dehosse@etu.univ-grenoble-alpes.fr>
2023-06-15 18:45:04 +0200

    fix automate bug (step)

--- info3.game/game/info3/game/automate/Automate.java
+++ info3.game/game/info3/game/automate/Automate.java
@@ -41,6 +41,13 @@ public class Automate {
             Condition cond = transition.cond;
             Action action = transition.action;
             if(source==e.state && cond.eval(e)){
+                if(action!=null){
+                //else do the action of the transition.
+                String direction=action.Direction;
+                action.exec(e, direction);
+                e.state=dest;
+                }
+                else{
                 //If we want to go to a random state
                 if(dest.name.equals("_"))
                     {
@@ -53,13 +60,10 @@ public class Automate {
                 else
                     e.state=dest;
                 //If there is no action, just exit the function
-                if(action==null)
-                    return;
-                //else do the action of the transition.
-                String direction=action.Direction;
-                action.exec(e, direction);
+                }
                 return;
             }
+        
         }
         System.out.println("State unescapable\n");
     }
