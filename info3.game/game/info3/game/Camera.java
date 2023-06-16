a5dee3fe60153d9222a156d5ce92701ac372eb1d
noahkohrs <noah@kohrs.fr>
2023-06-16 16:20:09 +0200

    Updating Camera draw options

--- info3.game/game/info3/game/Camera.java
+++ info3.game/game/info3/game/Camera.java
@@ -116,6 +116,8 @@ public class Camera {
     }
 
     static public void drawImage(Graphics g, BufferedImage img, int x, int y, int width, int height) {
+        x += Math.max(0, -width);
+        x += Math.max(0, -height);
         if (debugMode) {
             g.drawImage(img, x, y, width, height, null);
         } else {
@@ -128,9 +130,35 @@ public class Camera {
 
     }
 
+    static public void drawEntity(Entity e, Graphics g, boolean invertedX, boolean invertedY) {
+        BufferedImage img = e.getImage();
+        if (invertedX && invertedY) {
+            drawImage(g, img, e.x + img.getWidth(), e.y + img.getHeight(), -img.getWidth(), -img.getHeight());
+        } else if (invertedX) {
+            drawImage(g, img, e.x + img.getWidth(), e.y, -img.getWidth(), img.getHeight());
+        } else if (invertedY) {
+            drawImage(g, img, e.x, e.y + img.getHeight(), img.getWidth(), -img.getHeight());
+        } else {
+            drawImage(g, img, e.x, e.y, img.getWidth(), img.getHeight());
+        }
+    }
+
     static public void drawEntity(Entity e, Graphics g) {
         BufferedImage img = e.getImage();
         drawImage(g, img, e.x, e.y, img.getWidth(), img.getHeight());
+    }
+
+    static public void drawImage(Graphics g, BufferedImage img, int x, int y, int width, int height, boolean invertedX,
+            boolean invertedY) {
+        if (invertedX && invertedY) {
+            drawImage(g, img, x + width, y + height, -width, -height);
+        } else if (invertedX) {
+            drawImage(g, img, x + width, y, -width, height);
+        } else if (invertedY) {
+            drawImage(g, img, x, y + height, width, -height);
+        } else {
+            drawImage(g, img, x, y, width, height);
+        }
     }
 
     static public void drawRect(Graphics g, int x, int y, int width, int height) {
