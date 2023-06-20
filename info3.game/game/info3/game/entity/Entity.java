46f1b2241f9ddd745e16d649c1259dead9f9528d
noahkohrs <noah@kohrs.fr>
2023-06-20 15:18:48 +0200

    powerup + chhanging constructor en entities

--- info3.game/game/info3/game/entity/Entity.java
+++ info3.game/game/info3/game/entity/Entity.java
@@ -42,14 +42,9 @@ public abstract class Entity {
   long deltatime;
 
   public Entity(int x, int y, int team, String filename, int nrows, int ncols) throws IOException {
-    this.team = team;
-    this.x = x;
-    this.y = y;
+    this(x, y, team);
     this.view = new EntityView(filename, nrows, ncols, this);
-    this.automate = loadAutomate();
-    if (this.automate == null)
-      this.automate = GameSession.gameSession.defaultAutomate;
-    state = this.automate.initalState;
+    this.hitbox = new HitBox(this);
   }
 
   public Entity(int x, int y, int team) throws IOException {
@@ -61,18 +56,6 @@ public abstract class Entity {
       this.automate = GameSession.gameSession.defaultAutomate;
     state = this.automate.initalState;
   }
-
-  public Entity(int x, int y, int team, EntityView view) throws IOException {
-    this.team = team;
-    this.x = x;
-    this.y = y;
-    this.automate = loadAutomate();
-    if (this.automate == null)
-      this.automate = GameSession.gameSession.defaultAutomate;
-    state = this.automate.initalState;
-    this.view=view;
-  }
-
   private Automate loadAutomate() {
     System.out.println("Loading automate for " + this.getClass().getSimpleName());
     return GameSession.gameSession.findAutomate(this);
