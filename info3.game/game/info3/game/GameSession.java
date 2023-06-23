cba50cf0959b01080785c2af89586ad0a30f15a2
Noé Flechon <noe.flechon@etu.univ-grenoble-alpes.fr>
2023-06-23 15:54:30 +0200

    fix LifeBar

--- info3.game/game/info3/game/GameSession.java
+++ info3.game/game/info3/game/GameSession.java
@@ -222,10 +222,14 @@ public class GameSession {
         camera.paint(g);
         map.paint(g, camera);
         for (Entity entity : entities) {
-            if (entity instanceof Player && ((Player) entity).dead)
+            if (entity instanceof Player)
                 continue;
             entity.view.paint(g);
         }
+        if (!player1.dead)
+            player1.view.paint(g);
+        if (!player2.dead)
+            player2.view.paint(g);
         gametime.showGameTimer(g);
         ;
     }
